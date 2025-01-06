4541568c6a9210cf412c6b776c83f33b3faf562f
Jeong Jiyong <98jiyong@gmail.com>
2025-01-07 04:31:21 +0900

    20250107_0430_commit

--- EDA_weather_bicycle.py
+++ EDA_weather_bicycle.py
@@ -2,7 +2,7 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 # 데이터 불러오기
-df = pd.read_csv('dataset/Coffee_export.csv', encoding='cp949')
+df = pd.read_csv('dataset/weather_bicycle.csv', encoding='cp949')
 
 # 프로파일링 리포트 생성
 profile = ProfileReport(
